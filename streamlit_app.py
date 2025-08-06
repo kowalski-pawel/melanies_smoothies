aeee21fb8bc95d90916a917b4f0f0d28c1da2321
kowalski-pawel <36761059+kowalski-pawel@users.noreply.github.com>
2025-08-06 10:57:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,8 +12,9 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 name_on_order = st.text_input('Imie na zamowieniu:')
 
